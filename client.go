bb59ccc7dc6e3dbac15aba6c7a15bd7f5ff22078
shaoguixin <2404987607@qq.com>
2022-04-18 17:05:06 +0800

    实现Client菜单功能

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -10,6 +11,7 @@ type Client1 struct {
 	ServerProt int
 	Name       string
 	conn       net.Conn
+	flag       int //用于菜单的选择
 }
 
 func newClient1(Ip string, Port int) *Client1 {
@@ -17,6 +19,7 @@ func newClient1(Ip string, Port int) *Client1 {
 	client1 := &Client1{
 		ServerIp:   Ip,
 		ServerProt: Port,
+		flag:       999,
 	}
 	//连接Server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", Ip, Port))
@@ -30,8 +33,55 @@ func newClient1(Ip string, Port int) *Client1 {
 	return client1
 }
 
+var serverIp string
+var serverPort int
+
+//提供一个menu的方法
+func (this *Client1) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		this.flag = flag
+		return true
+	} else {
+		fmt.Println("=====请输入合法范围内的数字=====")
+		return false
+	}
+
+}
+
+func (this *Client1) Run() {
+	for this.flag != 0 {
+		for this.menu() != true {
+		} //没有选择就一直挂着
+		switch this.flag {
+		case 1:
+			fmt.Println("公聊模式选择。。。")
+		case 2:
+			fmt.Println("私聊模式选择。。。")
+		case 3:
+			fmt.Println("更新用户名选择。。。")
+		}
+	}
+}
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置Server的Ip，default:127.0.0.1")
+	flag.IntVar(&serverPort, "port", 8888, "设置Server的Port，default:8888")
+}
+
 func main() {
-	client := newClient1("127.0.0.1", 8888)
+
+	flag.Parse()
+
+	client := newClient1(serverIp, serverPort)
 
 	if client == nil {
 		fmt.Println("=====链接服务器失败=====")
@@ -39,6 +89,8 @@ func main() {
 	}
 
 	fmt.Println("=====链接服务器成功=====")
+
+	client.Run()
 
 	//暂时进行阻塞
 	select {}
