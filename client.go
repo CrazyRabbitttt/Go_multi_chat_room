2fa79a8412a9c329bcde61f64c099911712115a2
shaoguixin <2404987607@qq.com>
2022-04-18 18:05:11 +0800

    完结撒花🎉

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client1 struct {
@@ -57,17 +59,115 @@ func (this *Client1) menu() bool {
 
 }
 
+func (this *Client1) SelectUsers() {
+	//查询当前在线的用户
+	sendMsg := "who\n"
+
+	_, err := this.conn.Write([]byte(sendMsg))
+
+	if err != nil {
+		fmt.Println("conn Write err:", err)
+		return
+	}
+}
+
+func (this *Client1) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	this.SelectUsers()
+
+	fmt.Println("====请输入聊天对象[用户名],exit退出：")
+
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println("====请输入消息内容，exit退出:")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			//消息不空就发送
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + "\n\n"
+				_, err := this.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+			}
+
+			chatMsg = ""
+			fmt.Println("====请输入消息内容，exit退出:")
+			fmt.Scanln(&chatMsg)
+		}
+		this.SelectUsers()
+		fmt.Println("====请输入消息内容，exit退出:")
+		fmt.Scanln(&chatMsg)
+
+	}
+
+}
+
+func (this *Client1) PublicChat() {
+	//提示用户输入信息
+	var chatMsg string
+
+	fmt.Println("====请输入聊天内容，exit输出")
+
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		sendMsg := chatMsg + "\n"
+
+		_, err := this.conn.Write([]byte(sendMsg))
+
+		if err != nil {
+			fmt.Println("conn Write err:", err)
+			break
+		}
+
+		chatMsg = ""
+		fmt.Println("====请输入聊天内容，exit输出")
+
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+//提供用户名更新的功能
+func (this *Client1) UpdateName() bool {
+	fmt.Println("====请输入用户名：")
+	fmt.Scanln(&this.Name)
+
+	//将我们既定好的格式传输过去
+
+	sendMessage := "rename|" + this.Name + "\n"
+
+	_, err := this.conn.Write([]byte(sendMessage))
+
+	if err != nil {
+		fmt.Println("conn.Write error :", err)
+		return false
+	}
+
+	return true
+}
+
+//处理Server回送的消息，输出到标准输出
+func (this *Client1) DealResponse() {
+	io.Copy(os.Stdout, this.conn)
+}
+
 func (this *Client1) Run() {
 	for this.flag != 0 {
 		for this.menu() != true {
 		} //没有选择就一直挂着
 		switch this.flag {
 		case 1:
-			fmt.Println("公聊模式选择。。。")
+			this.PublicChat()
 		case 2:
-			fmt.Println("私聊模式选择。。。")
+			this.PrivateChat()
 		case 3:
-			fmt.Println("更新用户名选择。。。")
+			this.UpdateName()
 		}
 	}
 }
@@ -89,6 +189,8 @@ func main() {
 	}
 
 	fmt.Println("=====链接服务器成功=====")
+
+	go client.DealResponse()
 
 	client.Run()
 
